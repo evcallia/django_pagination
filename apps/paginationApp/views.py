ab4f3f44574195fc3e68f006458fb1dd72347a48
Evan <calliaevan12@gmail.com>
2016-12-13 14:38:30 -0800

    filtering on keydown

--- apps/paginationApp/views.py
+++ apps/paginationApp/views.py
@@ -6,12 +6,13 @@ from .models import User
 #CONTROLLER
 #Create your views here.
 def index(request):
-    # User.objects.create(firstName="Evan", lastName="Callia", email="soccer_evan4@hotmail.com")
-    # User.objects.create(firstName="asdf", lastName="Callfdsaia", email="soccer_evan4@hotmail.com")
-    # User.objects.create(firstName="Joe", lastName="Depew", email="depew@hotmail.com")
-    # User.objects.create(firstName="Jeff", lastName="Kwok", email="jeff@hotmail.com")
-    # User.objects.create(firstName="Laurel", lastName="Rice", email="rice@hotmail.com")
-    # User.objects.create(firstName="Fran", lastName="L", email="Fran@hotmail.com")
+    # User.objects.create(firstName="Symantha", lastName="Smith", email="sym@hotmail.com")
+    # User.objects.create(firstName="Victor", lastName="Gonza", email="victor@hotmail.com")
+    # User.objects.create(firstName="Mia", lastName="Shy", email="mia@hotmail.com")
+    # User.objects.create(firstName="Lola", lastName="Humphry", email="lola@hotmail.com")
+    # User.objects.create(firstName="Jennifer", lastName="Circo", email="jen@hotmail.com")
+    # User.objects.create(firstName="Hannah", lastName="Dolph", email="hannah@hotmail.com")
+    # User.objects.filter(id=2).delete()
     if(request.method == 'POST'):
         return render(request, 'paginationApp/index.html', User.objects.getUsers(request.POST))
     return render(request, 'paginationApp/index.html', User.objects.getUsers(None))
